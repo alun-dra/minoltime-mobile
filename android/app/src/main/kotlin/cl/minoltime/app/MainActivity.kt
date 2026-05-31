a39328a78976ceee5a20df57918da6d5a28cc0e5
alvaro villalobos <alvaro.villalobos@social-it.cl>
2026-05-30 22:52:26 -0400

    prod v2

---
+++ android/app/src/main/kotlin/cl/minoltime/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package cl.minoltime.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/minoltimemobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.minoltimemobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
